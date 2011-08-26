56f3bb426fdace679daab6eedb84e5cbfba037a4
phiggins <pete@peterhiggins.org>
2011-08-26 01:01:46 -0700

    Initial commit.
    `rails new dummy_testunit`
    Add therubyracer to Gemfile.
    `bundle`
    `rake db:migrate`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyTestunit::Application.config.secret_token = '9edd50259f515e0b851e9e944cf22514c64101c25b9bdd27ddbde8ae2952a20fec264172c32b87dcca6925590e456d3ffd28af8e64906c169b3ea3854b0cbd65'
